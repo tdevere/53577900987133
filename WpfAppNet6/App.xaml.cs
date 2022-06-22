48d84d1a33731774207220018510316dae51b6c8
Tony DeVere <tdevere@microsoft.com>
2022-06-22 14:07:11 -0500

    Add project files.

---
+++ WpfAppNet6/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+
+namespace WpfAppNet6
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        string userID = "YourUserId";
+        string appcenterSecret = "";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppCenter.SetUserId(userID);
+            AppCenter.LogLevel = LogLevel.Verbose;
+
+            //https://docs.microsoft.com/en-us/appcenter/sdk/analytics/windows#manage-start-session
+            Analytics.EnableManualSessionTracker();
+
+            AppCenter.Start(appcenterSecret, typeof(Analytics));
+
+            //Necessary in 4.5.1? Auto Tracked in previous version?
+            Analytics.StartSession();
+
+            Analytics.TrackEvent("App Center OnStartup Complete .NET 6");
+
+            base.OnStartup(e);
+        }
+    }
+}
